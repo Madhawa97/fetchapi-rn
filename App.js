85e6ea56be73712f09a9b52a1136b78ccdc49499
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-11-07 02:12:36 +0530

    Add code to fetch and display data

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    ActivityIndicator,
+    FlatList,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const movieURL = "https://reactnative.dev/movies.json";
+
+const App = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [title, setTitle] = useState([]);
+    const [description, setDescription] = useState([]);
+
+    // similar to 'componentDidMount', gets called once
+    useEffect(() => {
+        fetch(movieURL)
+            .then((response) => response.json()) // get response, convert to json
+            .then((json) => {
+                setData(json.movies);
+                setTitle(json.title);
+                setDescription(json.description);
+            })
+            .catch((error) => alert(error)) // display errors
+            .finally(() => setLoading(false)); // change loading state
+    }, []);
+
+	return (
+        <SafeAreaView style={styles.container}>
+            {/* While fetching show the indicator, else show response*/}
+            {isLoading ? (
+                <ActivityIndicator />
+            ) : (
+                <View>
+                    {/* Title from URL */}
+                    <Text style={styles.title}>{title}</Text>
+                    {/* Display each movie */}
+                    <View
+                        style={{ borderBottomWidth: 1, marginBottom: 12 }}
+                    ></View>
+                    <FlatList
+                        data={data}
+                        keyExtractor={({ id }, index) => id}
+                        renderItem={({ item }) => (
+                            <View style={{ paddingBottom: 10 }}>
+                                <Text style={styles.movieText}>
+                                    {item.id}. {item.title}, {item.releaseYear}
+                                </Text>
+                            </View>
+                        )}
+                    />
+                    {/* Show the description */}
+                    <Text style={styles.description}>{description}</Text>
+                </View>
+            )}
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 48,
+    },
+    movieText: {
+        fontSize: 26,
+        fontWeight: "200",
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: "bold",
+    },
+    description: {
+        textAlign: "center",
+        marginBottom: 18,
+        fontWeight: "200",
+        color: "green",
+    },
 });
+
+export default App;
